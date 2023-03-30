07f737b7fbd35e58b7ce56e6d4b80c83d881613f
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-03-30 11:11:26 +0700

    Update models.py

--- hostpital_management/hospital_quality_management/models.py
+++ hostpital_management/hospital_quality_management/models.py
@@ -5,6 +5,9 @@ from django.db import models
 
 # Tiêu chí đánh giá
 class EvaluationCriteria(models.Model):
-    index = models.CharField(max_length=4)
+    index_letter = models.CharField(max_length=4)
+    index_number1= models.CharField(max_length=4)
+    index_number2 = models.CharField(max_length=4)
     title = models.CharField(max_length=1000)
     legal_basis = models.TextField()
+    note = mo
