82eded5a3219546614cd86c32b2225b7c943fb40
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-03-30 11:11:20 +0700

    Update models.py

--- hostpital_management/hospital_quality_management/models.py
+++ hostpital_management/hospital_quality_management/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 # Models for Hospital Quality Management - HRM
+
+# Tiêu chí đánh giá
 class EvaluationCriteria(models.Model):
-    
+    index = models.CharField(max_length=4)
+    title = models.CharField(max_length=1000)
+    legal_basis = models.TextField()
