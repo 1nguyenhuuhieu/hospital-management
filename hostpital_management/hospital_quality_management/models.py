e6be2e44afc089d7ac4e42b7658b3ae5ff5b0f76
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-03-30 18:45:01 +0700

    Update models.py

--- hostpital_management/hospital_quality_management/models.py
+++ hostpital_management/hospital_quality_management/models.py
@@ -3,11 +3,14 @@ from django.db import models
 
 # Models for Hospital Quality Management - HRM
 
-# Tiêu chí đánh giá
+# Phần tiêu chí đánh giá
+class SectionCriteria(models.Model):
+    index = models.CharField(max_length=1)
+    title = models.CharField(max_length=200)
+
 class EvaluationCriteria(models.Model):
     index_letter = models.CharField(max_length=4)
     index_number1= models.CharField(max_length=4)
     index_number2 = models.CharField(max_length=4)
     title = models.CharField(max_length=1000)
     legal_basis = models.TextField()
-    note = mo
