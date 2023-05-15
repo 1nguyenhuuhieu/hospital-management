5f692c39cb22d649dfe64051b5c3fbbd230af74b
1nguyenhuuhieu <63792951+1nguyenhuuhieu@users.noreply.github.com>
2023-05-15 11:18:20 +0700

    fff

--- hostpital_management/account/forms.py
+++ hostpital_management/account/forms.py
@@ -4,9 +4,11 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField()
     password1 = forms.CharField(label='Mật khẩu', widget=forms.PasswordInput)
+    password2 = forms.CharField(label=None, widget=forms.PasswordInput, help_text=None)
+    email = forms.CharField(label='Email', widget=forms.HiddenInput)
+    username = forms.EmailField(label='Email', help_text="Địa chỉ email sẽ được dùng làm tên đăng nhập")
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1","password2"]
+        fields = ["username", "email", "password1", "password2"]
